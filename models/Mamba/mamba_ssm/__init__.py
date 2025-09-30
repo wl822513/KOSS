18ae3ba1a4b59f49c522326d14d7e7bfe42ec42b
wl822513 <147965608+wl822513@users.noreply.github.com>
2025-09-30 21:20:47 +0800

    Delete models directory

--- models/Mamba/mamba_ssm/__init__.py
+++
@@ -1,6 +0,0 @@
-__version__ = "2.2.4"
-
-from .ops.selective_scan_interface import selective_scan_fn, mamba_inner_fn
-from .modules.mamba_simple import Mamba
-from .modules.mamba2 import Mamba2
-from .models.mixer_seq_simple import MambaLMHeadModel
